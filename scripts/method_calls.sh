51139bf324f2da49ea17aedfd3d49696a4ff6cce
Harsh Rathi <rathiharsh2002@gmail.com>
2022-06-09 21:21:50 +0530

    add: generating deck done

---
+++ scripts/method_calls.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+near call game.teenpatti.testnet start_game --accountId harshrathi2511.testnet
--- teenpatti/src/contract.rs
+++ teenpatti/src/contract.rs
@@ -2,6 +2,7 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LookupMap, UnorderedSet};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{env, log, near_bindgen, AccountId, PanicOnDefault, Promise};
+// use ::phf::{phf_map, Map};
 
 // 5 Ⓝ in yoctoNEAR
 const INITIAL_BET: u128 = 5_000_000_000_000_000_000_000_000;
@@ -11,18 +12,51 @@ const SUITS: [&str; 4] = ["Spade", "Heart", "Club", "Diamond"];
 const CARD_TYPES: [&str; 13] = [
     "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A",
 ];
+const VALUES: [u16; 13] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
+// static KEYWORDS: phf::Map<&'static str, u16> = phf_map! {
+//     "loop" => 1,
+//     "continue" => 2,
+//     "break" => 3,
+//     "fn" => 4,
+//     "extern" => 5,
+// };
 // const VALUES:LookupMap<String,u16> = LookupMap::new(key_prefix: S)
 // need an efficient datastructure
 
-#[derive(BorshDeserialize, BorshSerialize, Debug)]
+#[derive(BorshDeserialize, BorshSerialize, Debug,Serialize,Deserialize)]
+#[serde(crate = "near_sdk::serde")]
 pub struct Deck {
     cards: Vec<Card>,
 }
 
 impl Deck {
-    pub fn new(&self) {
+    pub fn new() -> Self {
         // generate the 52 cards here
+        let mut hash_map: LookupMap<String, u16> = LookupMap::new(b"p");
+
+        // generate the lookup map
+        for (index, card) in CARD_TYPES.into_iter().enumerate() {
+            hash_map.insert(&card.to_string(), &VALUES[index]);
+        }
+
+        //generate the deck
+        let mut cards: Vec<Card> = Vec::new();
+        for suit in SUITS {
+            for card_type in CARD_TYPES {
+                let card = Card {
+                    suit: suit.to_string(),
+                    card_type: card_type.to_string(),
+                    value: hash_map
+                        .get(&card_type.to_string())
+                        .expect("index out of bound error"),
+                };
+                cards.insert(cards.len(), card);
+            }
+        }
+        
+        // generating the deck 
+        Deck { cards: cards }
     }
 }
 
@@ -47,7 +81,7 @@ enum SuitType {
 #[serde(crate = "near_sdk::serde")]
 pub struct Card {
     card_type: String,
-    suit: SuitType,
+    suit: String,
     value: u16,
 }
 
@@ -70,8 +104,14 @@ pub struct Game {
 
 #[near_bindgen]
 impl Game {
-    pub fn start_game() {
-        println!("Welcome to Teen Patti");
+
+    pub fn start_game()->Deck {
+        let deck = Deck::new();
+        return deck;
+    }
+
+    pub fn get_init_amount()->u128 {
+        INITIAL_BET
     }
 }
 
@@ -91,4 +131,29 @@ mod tests {
     }
 
     // TESTS HERE
+    #[test]
+    fn generate_deck() {
+        let mut hash_map: LookupMap<String, u16> = LookupMap::new(b"p");
+
+        // generate the lookup map
+        for (index, card) in CARD_TYPES.into_iter().enumerate() {
+            hash_map.insert(&card.to_string(), &VALUES[index]);
+        }
+
+        //generate the deck
+        let mut cards: Vec<Card> = Vec::new();
+        for suit in SUITS {
+            for card_type in CARD_TYPES {
+                let card = Card {
+                    suit: suit.to_string(),
+                    card_type: card_type.to_string(),
+                    value: hash_map
+                        .get(&card_type.to_string())
+                        .expect("index out of bound error"),
+                };
+                println!("{:?}", card);
+                cards.insert(cards.len(), card);
+            }
+        }
+    }
 }
