a72756abef08f9e88fff8dd793256d0f0a6a3b91
Harsh Rathi <rathiharsh2002@gmail.com>
2022-06-11 13:26:28 +0530

    add:view players in scripts and the contract

--- scripts/method_calls.sh
+++ scripts/method_calls.sh
@@ -1,11 +1,31 @@
 #!/bin/bash 
 
+echo ""
 near call game.teenpatti.testnet start_game --accountId harshrathi2511.testnet
 
-near call game.teenpatti.testnet register_player '{
-    "account_name": "harshrathi2511.testnet",
-    "name" : "Harsh Rathi",
-    ...
-    
-}' --accountId harshrathi2511.testnet 
+echo ""
+echo "registering players "
+near call game.teenpatti.testnet add_players '{
+    "input_players" : [
+        {
+            "account_id": "harshrathi2511.testnet",
+            "name": "Harsh Rathi"
+        },
+        {
+            "account_id": "samay200.testnet",
+            "name": "Samay Gandhi"
+        }
+    ] 
+}' --accountId teenpatti.testnet
+
+
+echo ""
+echo "view registered players .... "
+
+near view game.teenpatti.testnet get_players_data
+
+echo ""
+echo "for registering in the game ,each player needs to pay a collateral of .....NEAR "
+
+
 
