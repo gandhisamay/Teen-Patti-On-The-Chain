aa3af38d8f6a92d1f48d1e137b0e81b39e53c7ca
Harsh Rathi <rathiharsh2002@gmail.com>
2022-07-28 22:51:59 +0530

    fix: added pwd and error messages

--- teenpatti/src/init.rs
+++ teenpatti/src/init.rs
@@ -1,15 +1,19 @@
-use std::io::{self, Write};
-use text_io::read;
-use std::fs::File;
 use library::card::Card;
 use library::deck::Deck;
 use rand;
 use rand::seq::SliceRandom;
+use std::env;
+use std::fs::File;
+use std::io::{self, Write};
 use std::process::Command;
+use text_io::read;
 
-fn main() -> std::io::Result<()>{
+fn main() -> std::io::Result<()> {
     let mut file = File::create("add_players.sh")?;
-    file.write_all("near call gaming.teen_patti2.testnet add_players '{\n \"input_players\":  [\n".as_bytes()).unwrap();
+    file.write_all(
+        "near call gaming.teen_patti2.testnet add_players '{\n \"input_players\":  [\n".as_bytes(),
+    )
+    .unwrap();
     print!("Welcome to Teen Patti \n");
     io::stdout().flush().unwrap();
 
@@ -19,61 +23,66 @@ fn main() -> std::io::Result<()>{
 
     println!("Please enter your names!\n");
 
-    for i in 0..num{
-        println!("Player {}", i+1);
+    for i in 0..num {
+        println!("Player {}", i + 1);
         println!("Testnet Account Name.");
-        let account_id : String = read!();
+        let account_id: String = read!();
 
         println!("Player Name.");
-        let name : String = read!();
+        let name: String = read!();
 
         let mut message = String::new();
 
         let cards = generate_cards();
 
-        let card1_string = generate_card_string(cards.get(0).expect("Expected a card at index 0"), false);
-        let card2_string = generate_card_string(cards.get(1).expect("Expected a card at index 1"), false);
-        let card3_string = generate_card_string(cards.get(2).expect("Expected a card at index 2"), true);
+        let card1_string =
+            generate_card_string(cards.get(0).expect("Expected a card at index 0"), false);
+        let card2_string =
+            generate_card_string(cards.get(1).expect("Expected a card at index 1"), false);
+        let card3_string =
+            generate_card_string(cards.get(2).expect("Expected a card at index 2"), true);
 
-        if i+1 != num {
+        if i + 1 != num {
             message = format!("    {{\n      \"account_id\": \"{account_id}\",\n      \"name\": \"{name}\",\n      \"cards\": [\n{card1_string}{card2_string}{card3_string}      ]\n    }},\n");
-        }
-        else{
+        } else {
             message = format!("    {{\n      \"account_id\": \"{account_id}\",\n      \"name\": \"{name}\",\n      \"cards\": [\n{card1_string}{card2_string}{card3_string}      ]\n    }}\n");
         }
         println!();
         file.write_all(message.as_bytes()).unwrap();
     }
 
-    file.write_all("  ]\n}' --accountId teen_patti2.testnet".as_bytes()).unwrap();
+    file.write_all("  ]\n}' --accountId teen_patti2.testnet".as_bytes())
+        .unwrap();
 
-    Command::new("bash").arg("add_players.sh").current_dir("/home/samaygandhi/Teen-Patti-On-The-Chain").spawn().unwrap();
+    Command::new("bash")
+        .arg("add_players.sh")
+        .current_dir(env::current_dir().expect("Can\'t locate current directory"))
+        .spawn()
+        .expect("No directory for the following name found");
 
     Ok(())
-
 }
 
-pub fn generate_card_string(card: &Card, last_card: bool) -> String{
+pub fn generate_card_string(card: &Card, last_card: bool) -> String {
     let card_type = &card.card_type;
-    let suit  = &card.suit;
+    let suit = &card.suit;
     let value = card.value;
-    if last_card{
+    if last_card {
         let message = format!("        {{\n          \"card_type\": \"{card_type}\",\n          \"suit\": \"{suit}\",\n          \"value\": {value}    \n        }}\n");
         message
-    }
-    else{
+    } else {
         let message = format!("        {{\n          \"card_type\": \"{card_type}\",\n          \"suit\": \"{suit}\",\n          \"value\": {value}    \n        }},\n");
         message
     }
 }
 
-pub fn generate_cards() -> Vec<Card>{
+pub fn generate_cards() -> Vec<Card> {
     let deck = Deck::new();
     let hand: Vec<Card> = deck
-    .cards
-    .choose_multiple(&mut rand::thread_rng(), 3)
-    .cloned()
-    .collect();
-// Chooses amount elements from the slice at random, without repetition, and in random order.
+        .cards
+        .choose_multiple(&mut rand::thread_rng(), 3)
+        .cloned()
+        .collect();
+    // Chooses amount elements from the slice at random, without repetition, and in random order.
     hand
 }
